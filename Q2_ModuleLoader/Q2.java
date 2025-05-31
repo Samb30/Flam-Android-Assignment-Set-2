b44541b274f13e5c666d7f8b2b8bf196b4ebbda3
Samb30 <samanb3012@gmail.com>
2025-05-31 19:23:35 +0530

    Initial version graph built

--- Q2_ModuleLoader/Q2.java
+++ Q2_ModuleLoader/Q2.java
@@ -1,5 +1,34 @@
 package Q2_ModuleLoader;
 
-class Q1 {
-    
+import java.util.*;
+
+public class Q2 
+{
+    public static boolean dependency(int n, List<List<Integer>> edges) 
+    {
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) 
+        {
+            adj.add(new ArrayList<>());
+        }
+
+        for (List<Integer> edge : edges) 
+        {
+            int from = edge.get(0);
+            int to = edge.get(1);
+            adj.get(from).add(to);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) 
+    {
+        int n = 4;
+
+        List<List<Integer>> edges = Arrays.asList(Arrays.asList(0, 1), Arrays.asList(1, 2), Arrays.asList(2, 3));
+
+        System.out.println(dependency(n, edges));
+    }
 }
