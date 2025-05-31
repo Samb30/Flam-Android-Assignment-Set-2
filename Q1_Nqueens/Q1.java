a27c3119f1db510e1c1075cc25d356b231dd9061
Samb30 <samanb3012@gmail.com>
2025-05-31 18:52:44 +0530

    Optimized the time complexity

--- Q1_Nqueens/Q1.java
+++ Q1_Nqueens/Q1.java
@@ -2,81 +2,88 @@ package Q1_Nqueens;
 
 import java.util.*;
 
-public class Q1 {
-    public static List<List<String>> solveNQueens(int n) {
+public class Q1 
+{
+    public static List<List<String>> func(int n) 
+    {
         char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
-                board[i][j] = '.';
-        List<List<String>> res = new ArrayList<List<String>>();
-        dfs(0, board, res);
-        return res;
+        int[] lr = new int[n];
+        int[] ud = new int[2 * n - 1];
+        int[] ld = new int[2 * n - 1];
+        List<List<String>> r = new ArrayList<>();
+
+        for (char[] row : board)
+        {
+            Arrays.fill(row, '.');
+        }
+
+        solve(0, r, lr, board, ld, ud);
+
+        return r;
     }
 
-    static boolean validate(char[][] board, int row, int col) {
-        int duprow = row;
-        int dupcol = col;
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row--;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (col >= 0 && row < board.length) {
-            if (board[row][col] == 'Q')
-                return false;
-            col--;
-            row++;
-        }
-        return true;
-    }
-
-    static void dfs(int col, char[][] board, List<List<String>> res) {
-        if (col == board.length) {
-            res.add(construct(board));
+    static void solve(int col, List<List<String>> r, int[] lr, char[][] board, int[] ld, int[] ud) 
+    {
+        if (col == board.length) 
+        {
+            r.add(construct(board));
             return;
         }
 
-        for (int row = 0; row < board.length; row++) {
-            if (validate(board, row, col)) {
+        for (int row = 0; row < board.length; row++) 
+        {
+            if (lr[row] == 0 && ld[row + col] == 0 && ud[board.length - 1 + col - row] == 0) 
+            {
                 board[row][col] = 'Q';
-                dfs(col + 1, board, res);
+                lr[row] = 1;
+                ld[row + col] = 1;
+                ud[board.length - 1 + col - row] = 1;
+
+                solve(col + 1, r, lr, board, ld, ud);
+
                 board[row][col] = '.';
+                lr[row] = 0;
+                ld[row + col] = 0;
+                ud[board.length - 1 + col - row] = 0;
             }
         }
     }
 
-    static List<String> construct(char[][] board) {
-        List<String> res = new LinkedList<String>();
-        for (int i = 0; i < board.length; i++) {
-            String s = new String(board[i]);
-            res.add(s);
+    static List<String> construct(char[][] board) 
+    {
+        List<String> res = new ArrayList<>();
+        
+        for (char[] row : board)
+        {
+            res.add(new String(row));
         }
+        
         return res;
     }
 
-    public static void main(String args[]) {
-        int N = 4;
-        List<List<String>> queen = solveNQueens(N);
-        int i = 1;
-        for (List<String> it : queen) {
-            for (String s : it) {
-                System.out.println(s);
-            }
-            System.out.println();
-            i += 1;
-        }
+    public static void main(String[] args) 
+    {
+        int n = 4; // this value can be changed to test other inputs
 
+        List<List<String>> ans = func(n);
+
+        for (int i = 0; i < ans.size(); i++) 
+        {
+            List<String> board = ans.get(i);
+
+            for (int j = 0; j < board.size(); j++) 
+            {
+                System.out.print("\"" + board.get(j) + "\"");
+                if (j < board.size() - 1)
+                {
+                    System.out.print(",");
+                }
+            }
+
+            if (i < ans.size() - 1)
+            {
+                System.out.print("\n");
+            }
+        }
     }
 }
